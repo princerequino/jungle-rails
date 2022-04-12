5e91915e00ed13d737f2aabd4fa3a94bc2dda028
princerequino <requino.prince@gmail.com>
2022-04-11 21:56:34 -0400

    feature spec: product details // working rspec capybara test

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see product details when clicking from home page" do
+
+    visit root_path
+
+    within first(".product") do
+      click_link(match: :first)
+    end
+
+    # lets program wait for page load 
+    sleep 5
+
+    save_screenshot
+
+    expect(page).to have_css 'article.product-detail'
+    
+  end
+end
